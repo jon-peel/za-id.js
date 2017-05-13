20deaab60a4df91e86e0f9529c643a9c50b2fe5f
Jonathan Peel <me@jonathanpeel.co.za>
2017-05-13 11:55:33 +0200

    Validation working, with tests

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
-import * as dateFormat from 'dateformat';
+import * as moment from 'moment';
 
-export function validate(idNumber: string): boolean {
+const genderCheck = 5;
+const southAfrcianCheck = 10;
+const penultimate = 11;
+
+export interface idNumberDetails {
+	dateOfBirth?: Date;
+	southAfrican?: boolean;
+	gender?: boolean;
+}
+
+export function validateIdNumber(idNumber: string, details? :idNumberDetails): boolean {
 	if (idNumber.length !== 13 || !/^\d+$/.test(idNumber)) return false;
-	const birth = extarctBirthDate(idNumber);
-	if (!validateBirthdate(birth, idNumber)) return false;
+	const idArray = idNumber.split('').map(c => parseInt(c));
+	if (idArray[southAfrcianCheck] > 1 || idArray[penultimate] < 8 || !validateArray(idArray)) return false;
+	const dateOfBirth = extarctDateOfBirth(idNumber);
+	if (!validateBirthdate(dateOfBirth, idNumber)) return false;
+	if (details != null) {
+		details.dateOfBirth = dateOfBirth.toDate();
+		details.gender = idArray[genderCheck] >= 5;
+		details.southAfrican = idArray[southAfrcianCheck] === 0;
+	}	
 	return true;
 }
 
-function validateBirthdate(birth: Date, idNumber: string) {
-	return dateFormat(birth, "yymmdd") == idNumber.substr(0,6);
+function validateBirthdate(dateOfBirth: moment.Moment, idNumber: string) {
+	return dateOfBirth.format("YYMMDD") == idNumber.substr(0,6);
 }
 
-function extarctBirthDate(idNumber: string): Date {
- 	const now = new Date();
-	const year = parseInt(idNumber.substr(0, 2));
-	const month = parseInt(idNumber.substr(2, 2));
-	const day = parseInt(idNumber.substr(4, 2));	
-	let birthdate = new Date(2000 + year, month, day);
-	if (birthdate > now) birthdate = new Date(1900 + year, month - 1, day);
-	return birthdate;
+function validateArray(idArray: number[]) {
+	const odds = idArray.filter((n, i) => i % 2 === 0).reduce((a, b) => a + b, 0);
+    const evens = idArray.filter((n, i) => i % 2 !== 0).map(n => ((n *= 2) > 9) ? n - 9 : n).reduce((a, b) => a + b, 0);
+    const ncheck = odds + evens;
+    return (ncheck % 10) === 0;
+}
+
+function extarctDateOfBirth(idNumber: string): moment.Moment {
+ 	const now = moment();
+	const year = idNumber.substr(0, 2);
+	const month = idNumber.substr(2, 2);
+	const day = idNumber.substr(4, 2);
+	debugger;
+	let dateOfBirth = moment(`20${year}-${month}-${day} 00+0200`);
+	if (dateOfBirth.isAfter(now)) dateOfBirth = moment(`19${year}-${month}-${day} 00+0200`);
+	return dateOfBirth;
 }
