7eb3d8ec226a8ef31e5209bf7f53cd7afa592d13
Jonathan Peel <me@jonathanpeel.co.za>
2019-06-27 13:52:07 +0800

    fix some things

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,68 @@
 import * as moment from 'moment';
-import {filter, map, reduce} from 'lodash';
+import { filter, map, reduce } from 'lodash';
 
 export type gender = 'Male' | 'Female';
 export const male: gender = 'Male';
 export const female: gender = 'Female';
 
-const genderCheck = 5;
+const genderCheck = 6;
 const southAfricanCheck = 10;
 const penultimate = 11;
 
 export interface idNumberDetails {
-	dateOfBirth?: Date;
-	southAfrican?: boolean;
-	gender?: gender;
+  dateOfBirth?: Date;
+  southAfrican?: boolean;
+  gender?: gender;
 }
 
-export function validateIdNumber(idNumber: string, details? :idNumberDetails): boolean {
-	if (idNumber.length !== 13 || !/^\d+$/.test(idNumber)) return false;
-	const idArray = idNumber.split('').map(c => parseInt(c));
-	if (idArray[southAfricanCheck] > 1 || idArray[penultimate] < 8 || !validateArray(idArray)) return false;
-	const dateOfBirth = extractDateOfBirth(idNumber);
-	if (!validateBirthday(dateOfBirth, idNumber)) return false;
-	if (details != null) {
-		details.dateOfBirth = dateOfBirth.toDate();
-		details.gender = idArray[genderCheck] >= 5 ? male : female;
-		details.southAfrican = idArray[southAfricanCheck] === 0;
-	}	
-	return true;
+export function validateIdNumber(
+  idNumber: string,
+  details?: idNumberDetails
+): boolean {
+  if (idNumber.length !== 13 || !/^\d+$/.test(idNumber)) return false;
+  const idArray = idNumber.split('').map(c => parseInt(c));
+  if (
+    idArray[southAfricanCheck] > 1 ||
+    idArray[penultimate] < 8 ||
+    !validateArray(idArray)
+  )
+    return false;
+  const dateOfBirth = extractDateOfBirth(idNumber);
+  if (!validateBirthday(dateOfBirth, idNumber)) return false;
+  if (details != null) {
+    details.dateOfBirth = dateOfBirth.toDate();
+    details.gender = idArray[genderCheck] >= 5 ? male : female;
+    details.southAfrican = idArray[southAfricanCheck] === 0;
+  }
+  return true;
 }
 
 function validateBirthday(dateOfBirth: moment.Moment, idNumber: string) {
-	return dateOfBirth.format("YYMMDD") == idNumber.substr(0,6);
+  return dateOfBirth.format('YYMMDD') == idNumber.substr(0, 6);
 }
 
 function validateArray(idArray: number[]) {
-	const oddPredicate = (n, i: number) => i % 2 === 0;
-	const evenPredicate = (n, i: number) => i % 2 !== 0;
-	const evenMap = (n: number) => ((n *= 2) > 9) ? n - 9 : n;
-	const reduction = (a: number, b: number) => a + b;
-	const odds = reduce(filter(idArray, oddPredicate), reduction, 0);
-    const evens = reduce(map(filter(idArray,evenPredicate), evenMap), reduction, 0);
-    const nCheck = odds + evens;
-    return (nCheck % 10) === 0;
+  const oddPredicate = (n, i: number) => i % 2 === 0;
+  const evenPredicate = (n, i: number) => i % 2 !== 0;
+  const evenMap = (n: number) => ((n *= 2) > 9 ? n - 9 : n);
+  const reduction = (a: number, b: number) => a + b;
+  const odds = reduce(filter(idArray, oddPredicate), reduction, 0);
+  const evens = reduce(
+    map(filter(idArray, evenPredicate), evenMap),
+    reduction,
+    0
+  );
+  const nCheck = odds + evens;
+  return nCheck % 10 === 0;
 }
 
 function extractDateOfBirth(idNumber: string): moment.Moment {
- 	const now = moment();
-	const year = idNumber.substr(0, 2);
-	const month = idNumber.substr(2, 2);
-	const day = idNumber.substr(4, 2);
-	let dateOfBirth = moment(`20${year}-${month}-${day} 00+0200`);
-	if (dateOfBirth.isAfter(now)) dateOfBirth = moment(`19${year}-${month}-${day} 00+0200`);
-	return dateOfBirth;
+  const now = moment();
+  const year = idNumber.substr(0, 2);
+  const month = idNumber.substr(2, 2);
+  const day = idNumber.substr(4, 2);
+  let dateOfBirth = moment(`20${year}-${month}-${day} 00+0200`);
+  if (dateOfBirth.isAfter(now))
+    dateOfBirth = moment(`19${year}-${month}-${day} 00+0200`);
+  return dateOfBirth;
 }
--- src/test.ts
+++ src/test.ts
@@ -1,59 +1,61 @@
-import { expect, assert } from "chai";
+import { expect, assert } from 'chai';
 import {
   validateIdNumber as validate,
   idNumberDetails,
   gender,
   male,
   female
-} from "./index";
+} from './index';
 
 const data: id[] = [
-  { id: "8401295047080", dob: "1984-01-28T22:00:00.000Z", m: true, sa: true },
-  { id: "5309280385089", dob: "1953-09-27T22:00:00.000Z", m: true, sa: true },
-  { id: "6410170770080", dob: "1964-10-16T22:00:00.000Z", m: true, sa: true },
-  { id: "8412065666082", dob: "1984-12-05T22:00:00.000Z", m: true, sa: true },
-  { id: "7103060835084", dob: "1971-03-05T22:00:00.000Z", m: true, sa: true },
-  { id: "8302180857080", dob: "1983-02-17T22:00:00.000Z", m: true, sa: true },
-  { id: "5411270786084", dob: "1954-11-26T22:00:00.000Z", m: true, sa: true },
-  { id: "6212252193086", dob: "1962-12-24T22:00:00.000Z", m: true, sa: true },
-  { id: "8101270160086", dob: "1981-01-26T22:00:00.000Z", m: true, sa: true },
-  { id: "7202156107083", dob: "1972-02-14T22:00:00.000Z", m: true, sa: true },
-  { id: "1010270108085", dob: "2010-10-26T22:00:00.000Z", m: true, sa: true },
-  { id: "4812295009181", dob: "1948-12-28T22:00:00.000Z", m: true, sa: false },
-  { id: "7009110689084", dob: "1970-09-10T22:00:00.000Z", m: false, sa: true },
-  { id: "5503035365087", dob: "1955-03-02T22:00:00.000Z", m: false, sa: true },
-  { id: "7005025451081", dob: "1970-05-01T22:00:00.000Z", m: false, sa: true },
-  { id: "6911240308082", dob: "1969-11-23T22:00:00.000Z", m: false, sa: true },
-  { id: "5201045738084", dob: "1952-01-03T22:00:00.000Z", m: false, sa: true },
-  { id: "6108215079083", dob: "1961-08-20T22:00:00.000Z", m: false, sa: true },
-  { id: "4709020184083", dob: "1947-09-01T22:00:00.000Z", m: false, sa: true },
-  { id: "9805140397087", dob: "1998-05-13T22:00:00.000Z", m: false, sa: true },
-  { id: "9408300571086", dob: "1994-08-29T22:00:00.000Z", m: false, sa: true },
-  { id: "8205100657086", dob: "1982-05-09T22:00:00.000Z", m: false, sa: true },
-  { id: "4006300395087", dob: "1940-06-29T22:00:00.000Z", m: false, sa: true },
-  { id: "7207016182184", dob: "1972-06-30T22:00:00.000Z", m: false, sa: false },
-  { id: "7907175148088", dob: "1979-07-16T22:00:00.000Z", m: true, sa: true }
+  { id: '4006300395087', dob: '1940-06-29T22:00:00.000Z', m: false, sa: true },
+  { id: '4709020184083', dob: '1947-09-01T22:00:00.000Z', m: false, sa: true },
+  { id: '4812295009181', dob: '1948-12-28T22:00:00.000Z', m: true, sa: false },
+  { id: '5201045738084', dob: '1952-01-03T22:00:00.000Z', m: true, sa: true },
+  { id: '5309280385089', dob: '1953-09-27T22:00:00.000Z', m: false, sa: true },
+  { id: '5411270786084', dob: '1954-11-26T22:00:00.000Z', m: false, sa: true },
+  { id: '5503035365087', dob: '1955-03-02T22:00:00.000Z', m: true, sa: true },
+  { id: '6108215079083', dob: '1961-08-20T22:00:00.000Z', m: true, sa: true },
+  { id: '6212252193086', dob: '1962-12-24T22:00:00.000Z', m: false, sa: true },
+  { id: '6410170770080', dob: '1964-10-16T22:00:00.000Z', m: false, sa: true },
+  { id: '6911240308082', dob: '1969-11-23T22:00:00.000Z', m: false, sa: true },
+  { id: '7005025451081', dob: '1970-05-01T22:00:00.000Z', m: true, sa: true },
+  { id: '7009110689084', dob: '1970-09-10T22:00:00.000Z', m: false, sa: true },
+  { id: '7103060835084', dob: '1971-03-05T22:00:00.000Z', m: false, sa: true },
+  { id: '7202156107083', dob: '1972-02-14T22:00:00.000Z', m: true, sa: true },
+  { id: '7207016182184', dob: '1972-06-30T22:00:00.000Z', m: true, sa: false },
+  { id: '7408270297086', dob: '1974-08-26T22:00:00.000Z', m: false, sa: true },
+  { id: '7907175148088', dob: '1979-07-16T22:00:00.000Z', m: true, sa: true },
+  { id: '8101270160086', dob: '1981-01-26T22:00:00.000Z', m: false, sa: true },
+  { id: '8205100657086', dob: '1982-05-09T22:00:00.000Z', m: false, sa: true },
+  { id: '8302180857080', dob: '1983-02-17T22:00:00.000Z', m: false, sa: true },
+  { id: '8401295047080', dob: '1984-01-28T22:00:00.000Z', m: true, sa: true },
+  { id: '8412065666082', dob: '1984-12-05T22:00:00.000Z', m: true, sa: true },
+  { id: '8511180765081', dob: '1985-11-17T22:00:00.000Z', m: false, sa: true },
+  { id: '9408300571086', dob: '1994-08-29T22:00:00.000Z', m: false, sa: true },
+  { id: '9805140397087', dob: '1998-05-13T22:00:00.000Z', m: false, sa: true },
+  { id: '1010270108085', dob: '2010-10-26T22:00:00.000Z', m: false, sa: true }
 ];
 
-describe("za-id", () => {
-  describe("validate", () => {
-    it("empty string should be invalid", () =>
-      expect(validate("")).to.be.false);
-    it("does not contain any letters", () =>
-      expect(validate("802305047080a")).to.be.false);
-    it("invalid birthday should be invalid", () =>
-      expect(validate("8402305047080")).to.be.false);
-    it("valid number should be valid", () =>
-      expect(validate("8401295047080")).to.be.true);
+describe('za-id', () => {
+  describe('validate', () => {
+    it('empty string should be invalid', () =>
+      expect(validate('')).to.be.false);
+    it('does not contain any letters', () =>
+      expect(validate('802305047080a')).to.be.false);
+    it('invalid birthday should be invalid', () =>
+      expect(validate('8402305047080')).to.be.false);
+    it('valid number should be valid', () =>
+      expect(validate('8401295047080')).to.be.true);
 
-    it("birthday is Jan 29", () => {
+    it('birthday is Jan 29', () => {
       const details = {} as idNumberDetails;
-      var valid = validate("8401295047080", details);
+      var valid = validate('8401295047080', details);
       if (!details.dateOfBirth) {
-        return assert.fail(0, 1, "No date of birth returned");
+        return assert.fail(0, 1, 'No date of birth returned');
       }
       expect(details.dateOfBirth.toISOString()).to.equal(
-        "1984-01-28T22:00:00.000Z"
+        '1984-01-28T22:00:00.000Z'
       );
     });
 
@@ -61,15 +63,15 @@ describe("za-id", () => {
       const details = {} as idNumberDetails;
       var valid = validate(d.id, details);
       it(`${d.id} is valid`, () => expect(valid).to.be.true);
-      it(`${d.id} ${d.sa ? "is" : "is not"} South African`, () =>
+      it(`${d.id} ${d.sa ? 'is' : 'is not'} South African`, () =>
         expect(details.southAfrican).to.be.equal(d.sa));
       if (!details.gender) {
-        return assert.fail(0, 1, "No gender returned");
+        return assert.fail(0, 1, 'No gender returned');
       }
-      it(`${d.id} is ${d.m ? "male" : "female"}`, () =>
+      it(`${d.id} is ${d.m ? 'male' : 'female'}`, () =>
         expect((details.gender as gender) === male).to.be.equal(d.m));
       if (!details.dateOfBirth) {
-        return assert.fail(0, 1, "No date of birth returned");
+        return assert.fail(0, 1, 'No date of birth returned');
       }
       it(`${d.id} has correct date of birth`, () =>
         expect((details.dateOfBirth as Date).toISOString()).to.be.equal(d.dob));
